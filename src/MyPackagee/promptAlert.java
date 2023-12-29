8901bd41c65bbdb3925fe0b302f305600676a029
shikha <shikhasgahrwar149@gmail.com>
2023-12-29 22:55:52 +0530

    Prompt,Ri8Clk

---
+++ src/MyPackagee/promptAlert.java
@@ -0,0 +1,30 @@
+package MyPackagee;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class promptAlert {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\91922\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+
+		WebDriver driver= new ChromeDriver();
+
+		//maximise browser
+		driver.manage().window().maximize();
+
+		//open url
+		driver.get("https://www.hyrtutorials.com/p/alertsdemo.html");
+
+		//Alerts
+		//find alert button and perform click
+		driver.findElement(By.xpath("//button[@id='promptBox']")).click();
+
+		driver.switchTo().alert().sendKeys("Shikha");//enter text  on prompt button
+		//switch to alert window and accept the alert
+		driver.switchTo().alert().accept();
+	}
+
+}
